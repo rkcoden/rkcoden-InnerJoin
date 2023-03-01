81b804e5aa73c18bff4e84e69d6194531551a8a9
rkcoden <khanra4@farmingdale.edu>
2023-03-01 22:55:57 +0000

    DONE

--- problem1.sql
+++ problem1.sql
@@ -1,1 +1,1 @@
- SELECT student.id, student.student_name FROM class INNER JOIN student ON class.class_title = student.class_title WHERE class.teacher_name = student.student_name;
+SELECT student.id, student.student_name FROM class INNER JOIN student ON class.class_title = student.class_title WHERE class.teacher_name = 'Ms. Lovelace';
